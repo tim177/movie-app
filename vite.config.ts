377d91923a5aaff36e701bfaf0726c7c599a5bb9
tim177 <timaraw18@gmail.com>
2025-02-17 19:54:12 +0530

    improvment fixed pord 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",
+
   server: {
-    port: 5175,
+    fs: {
+      strict: false,
+    },
   },
+
   plugins: [react()],
   build: {
     outDir: "dist",
